fced902c5eead02668c2de363697d7aa7c600823
Andrea Aresu <107539841+AndreaAresu@users.noreply.github.com>
2026-07-04 17:19:04 +0200

    Step 3: encode_sentence_to_ids

--- model.py
+++ model.py
@@ -28,8 +28,11 @@ def build_id_to_token_vocab(token_to_id):
         id_to_token[v] = k
     return id_to_token
 
-# Step 3 - encode_sentence_to_ids (not yet solved)
-# TODO: implement
+# Step 3 - encode_sentence_to_ids
+def encode_sentence_to_ids(sentence, token_to_id, unk_token='<unk>'):
+    # TODO: convert whitespace tokens of `sentence` to ids via `token_to_id`, using `unk_token`'s id for OOV
+    
+    return [token_to_id.get(token, token_to_id[unk_token]) for token in sentence.split()]
 
 # Step 4 - decode_ids_to_tokens (not yet solved)
 # TODO: implement
