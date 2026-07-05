d4907e210c3b56ae9c78ff0ccefb89f30cf6897c
Andrea Aresu <107539841+AndreaAresu@users.noreply.github.com>
2026-07-05 23:56:59 +0200

    Step 10: fill_even_indices_with_sin

--- model.py
+++ model.py
@@ -74,7 +74,6 @@ import torch
 
 def fill_even_indices_with_sin(pe, position, div_term):
     """Fill even feature indices of pe with sin(position * div_term)."""
-    # TODO: write sin(position * div_term) into the even-indexed columns of pe and return it
     pe[:, 0::2] = torch.sin(position * div_term)
     return pe
 
